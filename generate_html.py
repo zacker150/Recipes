0f9da2e4d3055d2c79e3326766d69e126eaa4bf4
Victor Zeng <zacker150@hotmail.com>
2022-07-23 17:40:58 -0700

    Automatically generate links

---
+++ generate_html.py
@@ -0,0 +1,22 @@
+from glob import glob
+from io import TextIOWrapper
+from os import path
+
+from urllib import parse
+
+def write_link(filename: str, outfile: TextIOWrapper) -> None:
+    name = path.basename(filename)[0:-4]
+    filename = parse.quote(filename)
+    outfile.write(f'<a href="{filename}">{name}</a>\n')
+
+
+def main() -> None:
+    files = glob("Recipes/*.pdf")
+    
+    with open('recipe_links.html', 'w') as outfile:
+        for filename in files: 
+            write_link(filename, outfile)       
+
+
+if __name__ == "__main__":
+    main()
